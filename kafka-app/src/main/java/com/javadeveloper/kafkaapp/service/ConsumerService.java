cbc7232e575bf44abf252f551dc3a2b0c02cecef
Vivek Jain <javadeveloper.vivek@gmail.com>
2023-12-04 03:37:33 +0530

    change in logger.

--- kafka-app/src/main/java/com/javadeveloper/kafkaapp/service/ConsumerService.java
+++ kafka-app/src/main/java/com/javadeveloper/kafkaapp/service/ConsumerService.java
@@ -1,12 +1,11 @@
 package com.javadeveloper.kafkaapp.service;
 
-import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 
 public class ConsumerService {
-	private static final Logger logger = LoggerFactory.getLogger(KafkaConsumer.class);
+	private static final Logger logger = LoggerFactory.getLogger(ConsumerService.class);
 
 	@KafkaListener(topics = "${spring.kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
 	public void consume(String message){
